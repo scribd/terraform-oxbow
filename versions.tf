07cfaaf50d099f8cc15ec5148160205290f2581b
Oleh Motrunych <64472993+oleh-motrunych@users.noreply.github.com>
2024-01-11 14:38:34 +0100

    PE-4948: Delete versions.tf
    got issue with datadog initialisation

--- versions.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  required_providers {
-    # added datadog here because without it terrafrom is looking for provider in hashicorp/datadog - what is wrong
-    datadog = {
-      source = "datadog/datadog"
-    }
-  }
-}
