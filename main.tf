005b9f7cf3c41da878cb6a415bfc13115c262aa1
Hamilton Hord <hamiltonh@scribd.com>
2024-07-01 15:51:54 -0700

    Fixing Error in Terraform-data-warehouse prod deploy

--- main.tf
+++ main.tf
@@ -773,6 +773,8 @@ resource "aws_iam_policy" "glue_create_managed" {
 }
 
 resource "aws_iam_role" "glue_create" {
+  count = local.enable_glue_create ? 1 : 0
+
   name                = var.glue_create_config.iam_role_name
   assume_role_policy  = data.aws_iam_policy_document.glue_create_assume[0].json
   managed_policy_arns = [aws_iam_policy.glue_create_managed[0].arn]
