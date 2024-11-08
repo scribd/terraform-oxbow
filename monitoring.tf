c436e8016036161aa207351dfd77984059e45e67
Kuntal Basu <kuntalb@scribd.com>
2024-11-08 15:24:53 -0500

    Removing redundant local variables

--- monitoring.tf
+++ monitoring.tf
@@ -1,7 +1,4 @@
 locals {
-  enable_dead_letters_monitoring = var.enabled_dead_letters_monitoring
-  dl_warning                     = var.dl_warning
-  dl_critical                    = var.dl_critical
   dlq_to_monitor = [
     local.enable_group_events ? var.sqs_fifo_DL_queue_name : var.sqs_queue_name_dl,
     var.enable_glue_create ? var.glue_create_config.sqs_queue_name_dl : "",
@@ -10,7 +7,7 @@ locals {
 }
 
 resource "datadog_monitor" "dead_letters_monitor" {
-  for_each = local.enable_dead_letters_monitoring ? toset(local.dlq_to_monitor) : []
+  for_each = var.enable_dead_letters_monitoring ? toset(local.dlq_to_monitor) : []
 
   type = "metric alert"
   name = "${each.key}-monitor"
@@ -22,9 +19,9 @@ resource "datadog_monitor" "dead_letters_monitor" {
   query = "avg(last_1h):avg:aws.sqs.approximate_number_of_messages_visible{queuename:${each.key}} > ${var.dl_critical}"
 
   monitor_thresholds {
-    warning  = local.dl_warning
-    critical = local.dl_critical
-    ok       = local.dl_ok
+    warning  = var.dl_warning
+    critical = var.dl_critical
+    ok       = var.dl_ok
   }
 
   notify_no_data    = false
