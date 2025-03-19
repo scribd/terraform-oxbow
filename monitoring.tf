5c815f5c14bfcb4f9104b648339670956086acda
Kuntal Basu <kuntalb@scribd.com>
2025-03-19 12:56:51 -0400

    datadog only supports lower case

--- monitoring.tf
+++ monitoring.tf
@@ -1,8 +1,8 @@
 locals {
   dlq_to_monitor = [
-    local.enable_group_events ? "${var.sqs_fifo_DL_queue_name}.fifo" : var.sqs_queue_name_dl,
-    var.enable_glue_create ? var.glue_create_config.sqs_queue_name_dl : "",
-    var.enable_glue_sync ? var.glue_sync_config.sqs_queue_name_dl : "",
+    local.enable_group_events ? lower("${var.sqs_fifo_DL_queue_name}.fifo") : lower(var.sqs_queue_name_dl),
+    var.enable_glue_create ? lower(var.glue_create_config.sqs_queue_name_dl) : "",
+    var.enable_glue_sync ? lower(var.glue_sync_config.sqs_queue_name_dl) : "",
   ]
 }
 
